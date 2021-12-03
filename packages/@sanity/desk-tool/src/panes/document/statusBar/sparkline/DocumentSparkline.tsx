a2714367b82146647ede6b3d05ddf1e4c42e0c6b
hermanwikner <wiknerherman@hotmail.com>
2021-12-03 16:28:04 +0100

    feat(desk-tool): add updated `ReviewChangesButton` in `DocumentSparkline`

--- packages/@sanity/desk-tool/src/panes/document/statusBar/sparkline/DocumentSparkline.tsx
+++ packages/@sanity/desk-tool/src/panes/document/statusBar/sparkline/DocumentSparkline.tsx
@@ -1,74 +1,94 @@
-import {EditIcon} from '@sanity/icons'
 import {Box, Flex, useElementRect} from '@sanity/ui'
-import React, {useEffect, useMemo, useState, useRef, memo} from 'react'
-import {raf2} from '../../../../utils/raf'
+import React, {useEffect, useMemo, useState, useRef, memo, useCallback} from 'react'
+import {useSyncState} from '@sanity/react-hooks'
 import {useDocumentPane} from '../../useDocumentPane'
 import {DocumentBadges} from './DocumentBadges'
-import {ReviewChangesButton} from './ReviewChangesButton'
-import {IconBadge} from './IconBadge'
-import {
-  BadgesBox,
-  MetadataBox,
-  ReviewChangesBadgeBox,
-  ReviewChangesButtonBox,
-} from './DocumentSparkline.styled'
 import {PublishStatus} from './PublishStatus'
+import {ReviewChangesButton} from './ReviewChangesButton'
 
 export const DocumentSparkline = memo(function DocumentSparkline() {
-  const {editState, historyController, value} = useDocumentPane()
+  const {
+    editState,
+    historyController,
+    value,
+    documentId,
+    documentType,
+    handleHistoryClose,
+    handleHistoryOpen,
+    changesOpen,
+  } = useDocumentPane()
+  const syncState = useSyncState(documentId, documentType)
+
   const lastUpdated = value?._updatedAt
   const lastPublished = editState?.published?._updatedAt
   const showingRevision = historyController.onOlderRevision()
   const liveEdit = Boolean(editState?.liveEdit)
   const published = Boolean(editState?.published)
   const changed = Boolean(editState?.draft)
-  const loaded = published || changed
 
-  // Keep track of the size of the review changes button
-  const [
-    reviewChangesButtonElement,
-    setReviewChangesButtonElement,
-  ] = useState<HTMLButtonElement | null>(null)
-  const reviewChangesButtonElementRect = useElementRect(reviewChangesButtonElement)
-  const reviewChangesButtonWidth = reviewChangesButtonElementRect?.width
-  const reviewChangesButtonWidthRef = useRef(reviewChangesButtonWidth || 0)
+  const [rootFlexElement, setRootFlexElement] = useState<HTMLDivElement | null>(null)
+  const rootFlexRect = useElementRect(rootFlexElement)
+  const collapsed = Boolean(rootFlexRect && rootFlexRect?.width < 300)
 
-  // Use the size of the review changes button as a element query breakpoint
-  const metadataBoxBreakpoints = useMemo(
-    () => [reviewChangesButtonWidth || reviewChangesButtonWidthRef.current, 225],
-    [reviewChangesButtonWidth]
-  )
+  const initialState = changed ? 'changes' : undefined
+  const [status, setStatus] = useState<'changes' | 'saved' | 'syncing' | undefined>(initialState)
+  const changesTimer: {current: NodeJS.Timeout | null} = useRef(null)
+  const savedTimer: {current: NodeJS.Timeout | null} = useRef(null)
 
-  // Only transition between subsequent state, not the initial
-  const [transition, setTransition] = useState(false)
-  useEffect(() => {
-    if (!transition && loaded) {
-      // NOTE: the reason for double RAF here is a common "bug" in browsers.
-      // See: https://stackoverflow.com/questions/44145740/how-does-double-requestanimationframe-work
-      // There is no need to cancel this animation,
-      // since calling it again will cause the same result (transition=true).
-      return raf2(() => setTransition(true))
+  const handleSetStatus = useCallback((s) => setStatus(s), [])
+
+  const cancelAnimation = useCallback(() => {
+    if (savedTimer?.current && changesTimer?.current) {
+      clearInterval(changesTimer?.current)
+      clearInterval(savedTimer?.current)
     }
+  }, [savedTimer, changesTimer])
 
-    return undefined
-  }, [loaded, transition])
+  const runAnimation = useCallback(() => {
+    savedTimer.current = setTimeout(() => handleSetStatus('saved'), 1000)
+    changesTimer.current = setTimeout(() => handleSetStatus('changes'), 4000)
+  }, [handleSetStatus])
 
-  const metadataBoxStyle = useMemo(
-    () =>
-      ({
-        '--session-layout-width': reviewChangesButtonWidth
-          ? `${reviewChangesButtonWidth}px`
-          : undefined,
-      } as React.CSSProperties),
-    [reviewChangesButtonWidth]
-  )
-
-  // Maintain the last known width of the review changes button
+  // Reset status and cancel animation when changing document
   useEffect(() => {
-    if (reviewChangesButtonWidth) {
-      reviewChangesButtonWidthRef.current = reviewChangesButtonWidth
+    handleSetStatus(initialState)
+    setTimeout(() => {
+      cancelAnimation()
+    }, 1)
+  }, [cancelAnimation, handleSetStatus, initialState, documentId])
+
+  // 1. If is syncing, cancel animation and set the syncing status
+  // 2. When the syncing is complete, run the animation
+  useEffect(() => {
+    if (syncState.isSyncing) {
+      handleSetStatus('syncing')
+      cancelAnimation()
+    } else {
+      runAnimation()
     }
-  }, [reviewChangesButtonWidth])
+  }, [syncState.isSyncing, handleSetStatus, cancelAnimation, runAnimation])
+
+  const reviewButton = useMemo(
+    () => (
+      <ReviewChangesButton
+        lastUpdated={lastUpdated}
+        status={status}
+        onClick={changesOpen ? handleHistoryClose : handleHistoryOpen}
+        disabled={showingRevision}
+        selected={changesOpen}
+        collapsed={collapsed}
+      />
+    ),
+    [
+      changesOpen,
+      handleHistoryClose,
+      handleHistoryOpen,
+      lastUpdated,
+      showingRevision,
+      status,
+      collapsed,
+    ]
+  )
 
   const publishStatus = useMemo(
     () =>
@@ -79,56 +99,25 @@ export const DocumentSparkline = memo(function DocumentSparkline() {
             lastPublished={lastPublished}
             lastUpdated={lastUpdated}
             liveEdit={liveEdit}
+            collapsed={collapsed}
           />
         </Box>
       ),
-    [lastPublished, lastUpdated, liveEdit, published, showingRevision]
-  )
-
-  const metadata = useMemo(
-    () => (
-      <MetadataBox
-        data-changed={changed || liveEdit ? '' : undefined}
-        data-transition={transition ? '' : undefined}
-        media={metadataBoxBreakpoints}
-        style={metadataBoxStyle}
-      >
-        {!liveEdit && changed && (
-          <>
-            <ReviewChangesBadgeBox>
-              <IconBadge icon={EditIcon} muted tone="caution" />
-            </ReviewChangesBadgeBox>
-
-            <ReviewChangesButtonBox>
-              <ReviewChangesButton
-                disabled={showingRevision}
-                lastUpdated={lastUpdated}
-                ref={setReviewChangesButtonElement}
-              />
-            </ReviewChangesButtonBox>
-          </>
-        )}
-
-        <BadgesBox flex={1} marginLeft={3}>
-          <DocumentBadges />
-        </BadgesBox>
-      </MetadataBox>
-    ),
-    [
-      changed,
-      lastUpdated,
-      liveEdit,
-      metadataBoxBreakpoints,
-      metadataBoxStyle,
-      showingRevision,
-      transition,
-    ]
+    [collapsed, lastPublished, lastUpdated, liveEdit, published, showingRevision]
   )
 
   return (
-    <Flex align="center" data-ui="DocumentSparkline">
+    <Flex align="center" data-ui="DocumentSparkline" ref={setRootFlexElement}>
       {publishStatus}
-      {metadata}
+
+      <Flex align="center" flex={1}>
+        {reviewButton}
+        {!collapsed && (
+          <Box marginLeft={3}>
+            <DocumentBadges />
+          </Box>
+        )}
+      </Flex>
     </Flex>
   )
 })
